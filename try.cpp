476a744ff62f30981fefe76b2b94e7ef72c33c15
KSHITIJ NEERAJ VAZE <kshitijvaze@gmail.com>
2023-12-09 10:29:41 +0530

    Maintainence update

--- try.cpp
+++ try.cpp
@@ -1,23 +1,69 @@
 #include <iostream>
-#include <ctype.h>
 using namespace std;
 
-int main()
+class student
 {
-    char c;
-    cout << "Enter a character: ";
-    cin >> c;
-    if (isalpha(c))
+protected:
+    string name;
+    int prn;
+    string department;
+    int salary;
+
+public:
+    student(string _name, int _prn, string _dept, int _salary)
     {
-        cout << c << " is a letter." << endl;
-    }
-    else if (isdigit(c))
+        name = _name;
+        prn = _prn;
+        department = _dept;
+        salary = _salary;
+    };
+    student(){};
+    void setData()
     {
-        cout << c << " is a digit." << endl;
+        cin >> name >> prn >> department >> salary;
     }
-    else
+
+    void getData()
     {
-        cout << c << " is neither a letter nor a digit." << endl;
+        cout << name << endl
+             << prn << endl
+             << department << endl
+             << salary << endl;
     }
+};
+
+class library : public student
+{
+public:
+    library(string _name, int _prn, string _dept, int _salary) : student(_name, _prn, _dept, _salary){
+
+                                                                 };
+
+    int book_number;
+    string status;
+
+    void accept()
+    {
+        cout << "Enter the issued book number " << endl;
+        cin >> book_number;
+        cout << "Enter the status of the issuance" << endl;
+        cin >> status;
+    }
+
+    void display()
+    {
+    student:
+        getData();
+        cout << "The issued book Number is :- " << book_number << endl;
+        cout << "The status of the book is :- " << status << endl;
+    }
+};
+
+int main(void)
+{
+    library l("Kshitij", 123, "EnTC", 10000);
+    l.accept();
+    l.display();
+
     return 0;
 }
